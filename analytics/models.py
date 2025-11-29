39f41447d274e6ff97afc14116bdbcd90d67b0ac
kidus <kidus1219mail@gmail.com>
2025-11-29 11:55:25 +0300

    feat: create User, Blog and BlogView models

--- analytics/models.py
+++ analytics/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from blogs.models import Blog
 
-# Create your models here.
+class BlogView(models.Model):
+    blog = models.ForeignKey(Blog, related_name='views', on_delete=models.CASCADE)
+    ip_address = models.GenericIPAddressField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        db_table = "BlogView"
+
+    def __str__(self):
+        return f"{self.blog} - {self.ip_address} - {self.created_at}"
--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "Blog"
+
+    def __str__(self):
+        return f"{self.title} - {self.author} - {self.created_at}"
